2a70d79d50f8c1d2d4d83452c01d580d8637f153
jayme-github <tuxnet@gmail.com>
2015-08-24 10:40:34 +0200

    initial ui and dummy code

---
+++ main.py
@@ -0,0 +1,9 @@
+from PyQt4 import QtGui
+from ui.mainwindow import MainWindow
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    ui = MainWindow()
+    ui.show()
+    sys.exit(app.exec_())
