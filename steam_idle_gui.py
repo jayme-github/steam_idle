eab0541b168147c350017b1001ea435ca008ff33
jayme-github <tuxnet@gmail.com>
2015-10-05 20:13:45 +0200

    Suppress requests logging

--- steam_idle_gui.py
+++ steam_idle_gui.py
@@ -4,6 +4,7 @@ from PyQt4 import QtGui
 from steam_idle_qt.ui.mainwindow import MainWindow
 import logging
 logging.basicConfig(format='%(asctime)s (%(name)s.%(funcName)s) [%(levelname)s] %(message)s', level=logging.DEBUG)
+logging.getLogger('requests').setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 #for handler in logging.root.handlers:
 #    handler.addFilter(logging.Filter('steamweb'))
