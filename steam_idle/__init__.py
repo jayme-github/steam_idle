a3f4697df88825848cfbdea70bc018c9b58bddbe
jayme-github <tuxnet@gmail.com>
2015-12-23 12:53:37 +0100

    Add 'Holiday Sale 2015' appid to blacklist

--- steam_idle/__init__.py
+++ steam_idle/__init__.py
@@ -1,1 +1,5 @@
-GLOBAL_BLACKLIST = (368020, 335590) #TODO: Global blacklist should be online
+GLOBAL_BLACKLIST = (
+	368020,
+	335590,
+	425280, # Holiday Sale 2015
+) #TODO: Global blacklist should be online
