1862df482ca32c6aaaa22221378269c50c789c2c
jayme-github <tuxnet@gmail.com>
2016-02-26 16:01:43 +0100

    Adapt setup.py and readme

--- README.rst
+++ README.rst
@@ -1,30 +1,49 @@
-[![Code Health](https://landscape.io/github/jayme-github/steam_idle/master/landscape.svg?style=flat)](https://landscape.io/github/jayme-github/steam_idle/master)
+.. image:: https://landscape.io/github/jayme-github/steam_idle/master/landscape.svg?style=flat
+   :target: https://landscape.io/github/jayme-github/steam_idle/master
+   :alt: Code Health
 
+============
 Steam Idle
-===========
+============
 
-This is mostly a rewrite of [Idle Master (Python branch)](https://github.com/jshackles/idle_master_py).
+This is mostly a rewrite of `Idle Master (Python branch) <https://github.com/jshackles/idle_master_py>`_.
 It will idle all games with play time < 2 hours in parallel until they are out of the refund period (2+ hours).
 After that, it will idle each game sequentially untill all cards have been dropped.
 
 I did this rewrite because I don't wanted to poke around with cookies (and I thought idle_master_py is unmaintained).
 
+Installation
+============
+
+.. code-block:: sh
+
+    pip install steam_idle
+
+Requirements
+============
+
+* `steamweb <https://github.com/jayme-github/steamweb>`_>=0.6
+* pycrypto>=2.6.1
+* requests>=2.7.0
+* future>=0.14.3 (python 2.x)
+
 Usage
--------
+=====
 
-* Install [steamweb](https://github.com/jayme-github/steamweb)
-* Lauch Steam (and login)
-* Start `steam_idle_cli.py` for a command line interface
+Just run *steam_idle_cli.py* and follow the instructions:
 
-GUI Usage
--------
-* Install [steamweb](https://github.com/jayme-github/steamweb)
-* Install PyQt4
-* Start `steam_idle_gui.py` for a Qt4 GUI
+.. code-block:: sh
 
-I've only tested this with 64bit Linux so please report problems/bugs.
+    steam_idle_cli.py
+
+
+
+GUI (Qt) version
+================
+
+For a (Qt 4) GUI version please see `steam_idle_gui <https://github.com/jayme-github/steam_idle_gui>`_.
 
 License
--------
+=======
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation.  A copy of the GNU General Public License can be found at http://www.gnu.org/licenses/.  For your convenience, a copy of this license is included.
--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python
 
 from setuptools import setup
-import os
-import py2exe
-import PyQt4
-import requests.certs
-
-print(PyQt4.__path__)
 
 def parse_requirements(path):
     with open(path, 'r') as infile:
@@ -15,38 +9,29 @@ def parse_requirements(path):
 setup(
     name = 'steam_idle',
     version = '0.1',
-    description = 'Idle your Steam libraty for cards',
+    description = 'Idle Steam apps/games for card drops',
+    long_description = open('README.rst', 'r').read(),
+    platforms = ['any'],
+    keywords = 'steam',
     author = 'Jayme',
     author_email = 'tuxnet@gmail.com',
     url = 'https://github.com/jayme-github/steam_idle',
-    install_requires = parse_requirements('requirements.txt'),
+    license = 'GNU Affero General Public License v3',
     classifiers = [
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: GNU Affero General Public License v3',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
     ],
-    packages = [
-        'steam_idle',
-        'steam_idle_qt'
-    ],
-    data_files = [
-        ('libs', ['steam_idle/libs/steam_api64.dll',
-                    'steam_idle/libs/libsteam_api.so',
-                    'steam_idle/libs/steam_api.dll',
-                    'steam_idle/libs/libsteam_api64.so',
-                    'steam_idle/libs/libsteam_api.dylib']),
-        ('', [requests.certs.where(),]),
-        ('imageformats', [os.path.join(PyQt4.__path__[0], 'plugins', 'imageformats', 'qjpeg4.dll'),]),
-    ],
-    scripts = [
-        'steam_idle_cli.py',
-        'steam_idle_gui.py',
-    ],
-    windows = ['steam_idle_gui.py'],
-    options = {
-        'py2exe': {
-            'includes': ['sip', 'PyQt4.QtCore', 'dbm.dumb'],
-        },
+    install_requires = parse_requirements('requirements.txt'),
+    packages = ['steam_idle'],
+    package_dir = {'steam_idle': 'steam_idle'},
+    package_data = {'steam_idle':
+                    ['libs/steam_api64.dll',
+                    'libs/libsteam_api.so',
+                    'libs/steam_api.dll',
+                    'libs/libsteam_api64.so',
+                    'libs/libsteam_api.dylib']
     },
+    scripts = ['steam_idle_cli.py'],
 )
